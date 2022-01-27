35b7bb46d94d9f57680db21313226326ce305a47
Siva Rama Krishna Manyam <s544906@nwmissouri.edu>
2022-01-27 12:13:56 -0600

    Assignment2

---
+++ Assignments/Manyam_FormatName/Manyam_FormatName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manyam_FormatName
+//
+//  Created by Manyam,Siva Rama Krishna on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
